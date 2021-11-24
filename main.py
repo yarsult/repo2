8a64939dd7c524c5c2f83f7d49721238e671ca0d
yarsult <94232079+yarsult@users.noreply.github.com>
2021-11-24 20:52:20 +0300

    дизайн

--- main.py
+++ main.py
@@ -1,36 +1,61 @@
-c = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-n = input()
-n1 = []
-f = False
-f2 = False
-s = '+7'
-for i in n:
-    if i != ' ' and i != '\t' and i != '\n':
-        n1.append(i)
-try:
-    for i in n1:
-        assert not(i in c or i == '-' or i == '+' or i == '(' or i == ')')
-
-    for i in n1:
-        if i == '(' and not f2:
-            f2 = True
-        if i == ')' and f2:
-            f2 = False
-        elif i == ')':
-            f2 = True
-
-    assert n1[0] == '8':
-    n1 = ['+', '7'] + n1[1:]
+import sys
+import sqlite3
+from PyQt5 import uic
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QTableView
 
 
-if n1.count('(') == n1.count(')') and (n1[0] == '+' and n1[1] == '7' or n1[0] == '8') and '--' \
-        not in ''.join(n1) and n1[0] != '-' and n1[-1] != '-' and not f and not f2:
-    for i in n1[2:]:
-        if i in c:
-            s += i
-    if len(s) == 12:
-        print(s)
-    else:
-        print('error')
-else:
-    print('error')
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+
+        self.initUI()
+
+    def initUI(self):
+        self.bd()
+        db = QSqlDatabase.addDatabase('QSQLITE')
+        db.setDatabaseName('coffee.sqlite')
+        db.open()
+
+        view = self.tableView
+        model = QSqlTableModel(self, db)
+        model.setTable('coffee')
+        model.select()
+
+        view.setModel(model)
+        view.move(10, 10)
+        view.resize(617, 315)
+
+        self.setGeometry(300, 100, 650, 450)
+        self.setWindowTitle('Пример работы с QtSql')
+
+    def bd(self):
+        con = sqlite3.connect('coffee.sqlite')
+        cur = con.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS coffee(
+               ID INT PRIMARY KEY,
+               name TEXT,
+               degree_of_roasting TEXT,
+               consistency TEXT,
+               description TEXT,
+               price INT,
+               volume INT
+               );
+            """)
+
+        zn1 = [(1, 'Бразильский', 'средняя', 'молотый', 'вкусный', 150, 500), (2, 'Колумбийский', 'слабая', 'в зёрнах',
+                                                                               'не такой вкусный', 120, 600),
+               (3, 'Гавайский', 'сильная', 'в зёрнах', 'невкусный', 110, 550), (4, 'Индийский', 'средняя', 'молотый',
+                                                                                'очень вкусный', 300, 450)]
+        zp1 = """INSERT OR IGNORE INTO coffee(ID, name, degree_of_roasting, consistency, description, price, volume) \
+        VALUES(?, ?, ?, ?, ?, ?, ?)"""
+        cur.executemany(zp1, zn1)
+        con.commit()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
