48fd90a69d4527b6874d2aaa61086b1299c17b7d
yarsult <94232079+yarsult@users.noreply.github.com>
2021-11-28 17:41:55 +0300

    Капучино

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import sqlite3
 from PyQt5 import uic
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QTableView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 
 
 class Example(QMainWindow):
@@ -28,7 +28,7 @@ class Example(QMainWindow):
         view.resize(617, 315)
 
         self.setGeometry(300, 100, 650, 450)
-        self.setWindowTitle('Пример работы с QtSql')
+        self.setWindowTitle('Кофе')
 
     def bd(self):
         con = sqlite3.connect('coffee.sqlite')
@@ -53,9 +53,58 @@ class Example(QMainWindow):
         cur.executemany(zp1, zn1)
         con.commit()
 
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.update_result)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton_2.clicked.connect(self.save_results)
+        self.modified = {}
+        self.titles = None
+        self.setWindowTitle('Кофе')
+
+    def update_result(self):
+        cur = self.con.cursor()
+        result = cur.execute("SELECT * FROM coffee WHERE id=?",
+                             (item_id := self.spinBox.text(),)).fetchall()
+        self.tableWidget.setRowCount(len(result))
+        if not result:
+            self.statusBar().showMessage('Ничего не нашлось')
+            return
+        else:
+            self.statusBar().showMessage(f"Нашлась запись с id = {item_id}")
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        # Заполнили таблицу полученными элементами
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
+
+    def item_changed(self, item):
+        # Если значение в ячейке было изменено,
+        # то в словарь записывается пара: название поля, новое значение
+        self.modified[self.titles[item.column()]] = item.text()
+
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            que = "UPDATE coffee SET\n"
+            que += ", ".join([f"{key}='{self.modified.get(key)}'"
+                              for key in self.modified.keys()])
+            que += "WHERE id = ?"
+            print(que)
+            cur.execute(que, (self.spinBox.text(),))
+            self.con.commit()
+            self.modified.clear()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
+    ex1 = MyWidget()
+    ex1.show()
     sys.exit(app.exec())
